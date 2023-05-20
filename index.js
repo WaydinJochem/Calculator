3cfd88124d26196c7873913e9b77b132d1115055
WaydinJochem <jochem7405@gmail.com>
2023-05-20 18:08:09 +0200

    Values

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 
 
-let  InputValue= document.querySelector("#test");
+const  inputValue= document.querySelector("#test");
+const clearValue= document.querySelector(".clear");
+const deleteValue= document.querySelector(".delete");
+const multipyValue= document.querySelector(".multiply");
+const substractValue= document.querySelector(".subtract");
+const additionValue= document.querySelector(".addition");
+const totalValue= document.querySelector(".total");
 
 function nomb(number) {
-    InputValue.value = InputValue.value + number;
+    inputValue.value = inputValue.value + number;
+}
+function plus() {
+}
+function minus() {
+    
+}
+function multiply() {
+
+}
+function sum () {
+    totalValue.value = eval()
 }
